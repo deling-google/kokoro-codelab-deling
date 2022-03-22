f8afaa9bea16cf20b8b602bd85dbaa3fd10cee15
Deling Ren <cookiejar.seattle@gmail.com>
2022-03-21 22:00:52 -0700

    Kokoro codelab prepwork

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+  public static void main(String[]) {
+    System.out.println("Hello Kokoro");
+  }
+}
+
---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+javac Hello.java
+
+java Hello
+
