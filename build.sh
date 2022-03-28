fe8beb583ab257fdb02df93c588effaad815b065
deling-google <99381034+deling-google@users.noreply.github.com>
2022-03-28 11:31:55 -0700

    Try debugging

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 set -e
 
 cat debug_ssh_key_file >> ~/.ssh/authorized_keys
-# external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-# echo "INSTANCE_EXTERNAL_IP=${external_ip}"
+external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+echo "INSTANCE_EXTERNAL_IP=${external_ip}"
 
 sleep 60
 
