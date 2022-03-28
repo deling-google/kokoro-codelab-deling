83501e702b83cd4ade0c507f97b9fac9ee2c188c
deling-google <99381034+deling-google@users.noreply.github.com>
2022-03-28 12:05:49 -0700

    Try debugging

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 set -e
 
 cat debug_ssh_key_file >> ~/.ssh/authorized_keys
-# curl https://www.google.com
-curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip
+# curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip
+curl -4 https://whatismyip.com
 
 # external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
 # external_ip=$(curl -4 https://icanhazip.com)
