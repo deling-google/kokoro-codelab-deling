d26d5e3ec536cc4eaddece28469906205530496d
deling-google <99381034+deling-google@users.noreply.github.com>
2022-03-28 11:16:51 -0700

    Try debugging

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 
 set -e
 
-cat debug_ssh_key_file >> ~/.ssh/authorized_keys
-external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-echo "INSTANCE_EXTERNAL_IP=${external_ip}"
+# cat debug_ssh_key_file >> ~/.ssh/authorized_keys
+# external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+# echo "INSTANCE_EXTERNAL_IP=${external_ip}"
 
-sleep 60000
+sleep 60
 
 javac Hello.java
 
