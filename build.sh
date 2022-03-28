1a7d959fc9f08c9c7c16261494eab8c634ecc849
deling-google <99381034+deling-google@users.noreply.github.com>
2022-03-28 14:43:43 -0700

    Check if we have SPM

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@
 set -e
 
 cat debug_ssh_key_file >> ~/.ssh/authorized_keys
-external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-# external_ip=$(curl -4 https://icanhazip.com)
-echo "INSTANCE_EXTERNAL_IP=${external_ip}"
+# external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+# echo "INSTANCE_EXTERNAL_IP=${external_ip}"
 
-sleep 60
+# sleep 60
+
+swift run Dealer
 
 javac Hello.java
 
